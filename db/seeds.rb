49d6234ed941d6fe56ec8ee22516230ad6a0b199
akbarbin <muhamad.akbar@bfoxint.com>
2025-01-24 07:59:44 +0700

    [3] Generate User model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+["Reed Greenholt", "Reyes Kerluke", "Reuben Blick", "Brice Aufderhar"].each do |name|
+  User.find_or_create_by!(name: name)
+end
