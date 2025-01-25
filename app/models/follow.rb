536ed77ee8a3a3623d906e39dbf63f0da96e6260
akbarbin <muhamad.akbar@bfoxint.com>
2025-01-25 21:32:39 +0700

    [11] Validate to follow yourself

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,4 +1,12 @@
 class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+
+  validate :cannot_follow_yourself
+
+  private
+
+  def cannot_follow_yourself
+    errors.add(:followed, "You cannot follow yourself") if follower_id == followed_id
+  end
 end
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -3,4 +3,12 @@ require 'rails_helper'
 RSpec.describe Follow, type: :model do
   it { should belong_to(:follower) }
   it { should belong_to(:followed) }
+
+  it 'is invalid to follow yourself' do
+    user = create(:user)
+    follow = build(:follow, follower: user, followed: user)
+
+    expect(follow).not_to be_valid
+    expect(follow.errors[:followed]).to include("You cannot follow yourself")
+  end
 end
