89e49430122a1d453a8d1af304115886adece8ef
akbarbin <muhamad.akbar@bfoxint.com>
2025-01-25 10:39:38 +0700

    [6] Refactor records spec

--- spec/api/v1/user/records_spec.rb
+++ spec/api/v1/user/records_spec.rb
@@ -1,27 +1,53 @@
 require "rails_helper"
 
 RSpec.describe "Api::V1::User::Records", type: :request do
+  let(:headers) { { "Authorization" => valid_token } }
+  let(:valid_token) { "valid_token" }
+  let(:invalid_token) { "invalid_token" }
+
+  shared_examples_for "no_token_error" do
+    context "when no token is provided" do
+      let(:headers) { nil }
+
+      include_examples "user_not_found_error"
+    end
+  end
+
+  shared_examples_for "invalid_token_error" do
+    context "when an invalid token is provided" do
+      let(:headers) { { "Authorization" => invalid_token } }
+
+      include_examples "user_not_found_error"
+    end
+  end
+
+  shared_examples_for "user_not_found_error" do
+    it "returns an error" do
+      expect(response).to have_http_status(:unauthorized)
+      json_response = JSON.parse(response.body)
+      expect(json_response["message"]).to eq("Unauthorized or user not found")
+    end
+  end
+
   describe "GET /api/v1/user/records" do
-    let!(:user) { create(:user_with_records, records_count: 5, token: "valid_token") }
+    subject(:records_request) { get "/api/v1/user/records", headers: headers }
+
+    let!(:user) { create(:user_with_records, records_count: 5, token: valid_token) }
+
+    before { subject }
 
     context "when a valid token is provided" do
-      it "returns a list of records" do
-        get "/api/v1/user/records", headers: { "Authorization" => "valid_token" }
-
+      it "returns a list of records ordered by created time" do
         expect(response).to have_http_status(:ok)
         json_response = JSON.parse(response.body)
         expect(json_response.size).to eq(5)
-
-        # Check ascending order
         expect(json_response[0]["id"]).to eq(user.records.first.id)
       end
 
       context "when user has no records" do
-        let(:user) { create(:user, token: "valid_token") }
+        let(:user) { create(:user, token: valid_token) }
 
         it "returns an empty list" do
-          get "/api/v1/user/records", headers: { "Authorization" => "valid_token" }
-
           expect(response).to have_http_status(:ok)
           json_response = JSON.parse(response.body)
           expect(json_response.size).to eq(0)
@@ -29,34 +55,20 @@ RSpec.describe "Api::V1::User::Records", type: :request do
       end
     end
 
-    context "when an invalid token is provided" do
-      it "returns an error" do
-        get "/api/v1/user/records", headers: { "Authorization" => "invalid_token" }
-
-        expect(response).to have_http_status(:unauthorized)
-        json_response = JSON.parse(response.body)
-        expect(json_response["message"]).to eq("Unauthorized or user not found")
-      end
-    end
-
-    context "when no token is provided" do
-      it "returns an error" do
-        get "/api/v1/user/records"
-
-        expect(response).to have_http_status(:unauthorized)
-        json_response = JSON.parse(response.body)
-        expect(json_response["message"]).to eq("Unauthorized or user not found")
-      end
-    end
+    include_examples "invalid_token_error"
+    include_examples "no_token_error"
   end
 
   describe "POST /api/v1/user/records/clock_in" do
-    let!(:user) { create(:user, token: "valid_token") }
+    subject(:records_request) { post path, headers: headers }
+
+    let!(:user) { create(:user, token: valid_token) }
+    let(:path) { "/api/v1/user/records/clock_in" }
+
+    before { subject }
 
     context "when a valid token is provided" do
       it "creates a clock in record" do
-        post "/api/v1/user/records/clock_in", headers: { "Authorization" => "valid_token" }
-
         expect(response).to have_http_status(:created)
         json_response = JSON.parse(response.body)
         expect(json_response["id"]).not_to be_nil
@@ -65,33 +77,14 @@ RSpec.describe "Api::V1::User::Records", type: :request do
       end
     end
 
-    context "when an invalid token is provided" do
-      it "returns an error" do
-        post "/api/v1/user/records/clock_in", headers: { "Authorization" => "invalid_token" }
-
-        expect(response).to have_http_status(:unauthorized)
-        json_response = JSON.parse(response.body)
-        expect(json_response["message"]).to eq("Unauthorized or user not found")
-      end
-    end
-
-    context "when no token is provided" do
-      it "returns an error" do
-        post "/api/v1/user/records/clock_in"
-
-        expect(response).to have_http_status(:unauthorized)
-        json_response = JSON.parse(response.body)
-        expect(json_response["message"]).to eq("Unauthorized or user not found")
-      end
-    end
+    include_examples "invalid_token_error"
+    include_examples "no_token_error"
 
     context "when record is failed to save" do
-      before do
-        allow_any_instance_of(Record).to receive(:save).and_return(false)
-      end
+      before { allow_any_instance_of(Record).to receive(:save).and_return(false) }
 
       it "returns a 422 error with error message" do
-        post "/api/v1/user/records/clock_in", headers: { "Authorization" => "valid_token" }
+        post path, headers: headers
 
         expect(response).to have_http_status(:unprocessable_entity)
         json_response = JSON.parse(response.body)
@@ -101,13 +94,15 @@ RSpec.describe "Api::V1::User::Records", type: :request do
   end
 
   describe "POST /api/v1/user/records/clock_out" do
+    subject(:records_request) { post "/api/v1/user/records/clock_out", headers: headers }
+
     let!(:user) { create(:user, token: "valid_token") }
     let!(:record) { create(:clocked_in_record, user: user) }
 
     context "when a valid token is provided" do
-      it "saves a clock out from previous clock in record" do
-        post "/api/v1/user/records/clock_out", headers: { "Authorization" => "valid_token" }
+      before { subject }
 
+      it "saves a clock out from previous clock in record" do
         expect(response).to have_http_status(:created)
         json_response = JSON.parse(response.body)
         expect(json_response["id"]).not_to be_nil
@@ -116,11 +111,9 @@ RSpec.describe "Api::V1::User::Records", type: :request do
       end
 
       context "when previous clock in record is not found" do
-        let(:record) { nil }
+        let!(:record) { create(:record, clock_in_at: nil, user: user) }
 
         it "returns a 422 error with error message" do
-          post "/api/v1/user/records/clock_out", headers: { "Authorization" => "valid_token" }
-
           expect(response).to have_http_status(:not_found)
           json_response = JSON.parse(response.body)
           expect(json_response["message"]).to eq("No active clock in record")
@@ -129,33 +122,28 @@ RSpec.describe "Api::V1::User::Records", type: :request do
     end
 
     context "when an invalid token is provided" do
-      it "returns an error" do
-        post "/api/v1/user/records/clock_out", headers: { "Authorization" => "invalid_token" }
+      let(:headers) { { "Authorization" => invalid_token } }
 
-        expect(response).to have_http_status(:unauthorized)
-        json_response = JSON.parse(response.body)
-        expect(json_response["message"]).to eq("Unauthorized or user not found")
-      end
+      before { subject }
+
+      include_examples "user_not_found_error"
     end
 
     context "when no token is provided" do
-      it "returns an error" do
-        post "/api/v1/user/records/clock_out"
+      let(:headers) { nil }
 
-        expect(response).to have_http_status(:unauthorized)
-        json_response = JSON.parse(response.body)
-        expect(json_response["message"]).to eq("Unauthorized or user not found")
-      end
+      before { subject }
+
+      include_examples "user_not_found_error"
     end
 
     context "when record is failed to save" do
       before do
         allow_any_instance_of(Record).to receive(:update).and_return(false)
+        subject
       end
 
       it "returns a 422 error with error message" do
-        post "/api/v1/user/records/clock_out", headers: { "Authorization" => "valid_token" }
-
         expect(response).to have_http_status(:unprocessable_entity)
         json_response = JSON.parse(response.body)
         expect(json_response["message"]).to eq("Failed to clock out")
--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :record do
     clock_in_at { "2025-01-24 14:10:14" }
-    clock_out_at { "2025-01-24 14:10:14" }
+    clock_out_at { "2025-01-25 14:10:14" }
     user
-  end
 
-  factory :clocked_in_record, class: Record do
-    clock_in_at { "2025-01-24 14:10:14" }
-    user
+    factory :clocked_in_record, class: Record do
+      clock_in_at { "2025-01-24 14:10:14" }
+      clock_out_at { nil }
+    end
   end
 end
