c229084f79e8c842beb6508d75e26a8f7d426f7c
akbarbin <muhamad.akbar@bfoxint.com>
2025-01-26 13:26:04 +0700

    [14] Validate to not follow user twice

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -2,6 +2,8 @@ class Follow < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
 
+  validates :follower_id, uniqueness: { scope: :followed_id, message: "You cannot follow the same user twice" }
+
   validate :cannot_follow_yourself
 
   private
--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,10 +1,20 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Follow, type: :model do
   it { should belong_to(:follower) }
   it { should belong_to(:followed) }
 
-  it 'is invalid to follow yourself' do
+  it "is invalid to follow the same followed user twice" do
+    followed_user = create(:user)
+    user = create(:user)
+    first_follow = create(:follow, follower: user, followed: followed_user)
+    second_follow = build(:follow, follower: user, followed: followed_user)
+
+    expect(second_follow).not_to be_valid
+    expect(second_follow.errors[:follower_id]).to include("You cannot follow the same user twice")
+  end
+
+  it "is invalid to follow yourself" do
     user = create(:user)
     follow = build(:follow, follower: user, followed: user)
 
